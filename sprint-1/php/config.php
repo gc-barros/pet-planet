17442e957408ad4251300f19cda9b91711efadbf
Thiago Aires <thiagoaires.o@gmail.com>
2021-09-08 09:39:44 -0300

    'main'

---
+++ sprint-1/php/config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DBSERVER', 'localhost'); // Data server
+define('DBUSERNAME', 'root'); // Database username
+define('DBPASSWORD', ''); // Database password
+define('DBNAME', 'testeBD'); // Database name
+
+/*connect to MySQL database */
+$db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
+
+//Check db connection
+if($db === false){
+    die("Error: connection error. ". mysqli_connect_error());
+}
+?>
