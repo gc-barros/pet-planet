f7479f35d7f5b371c894fb67caf5bdeb05afad73
Gabriel Barros <87779356+gc-barros@users.noreply.github.com>
2021-09-12 00:52:54 -0300

    Updates

--- sprint-1/landingpage/js/menu-mobile.js
+++ sprint-1/landingpage/js/menu-mobile.js
@@ -1,35 +1,10 @@
 window.onload = function () {
     document.querySelector(".menuMobile").addEventListener("click", function () {
-
-        if (document.querySelector(".menu nav ul").style.display == 'flex') {
-            document.querySelector(".menu nav ul").style.display = 'none';
-
-            if (document.querySelector("#wave")) {
-                document.querySelector("#wave").style.display = 'flex';
-            }
-            
-            if (document.querySelector(".imagem-central")) {
-                document.querySelector(".imagem-central").style.display = 'flex';
-            }
-
-            if (document.querySelector(".logo-central")) {
-                document.querySelector(".logo-central").style.display = 'flex';
-            }
-
+        let visibleMenu = document.querySelector(".menu nav ul");
+        if (visibleMenu.style.display == 'flex') {
+            visibleMenu.style.display = 'none';
         } else {
-            document.querySelector(".menu nav ul").style.display = 'flex';
-
-            if (document.querySelector("#wave")) {
-                document.querySelector("#wave").style.display = 'none';
-            }
-
-            if (document.querySelector(".imagem-central")) {
-                document.querySelector(".imagem-central").style.display = 'none';
-            }
-           
-            if (document.querySelector(".logo-central")) {
-                document.querySelector(".logo-central").style.display = 'none';
-            }
+            visibleMenu.style.display = 'flex';
         }
     });
 };
